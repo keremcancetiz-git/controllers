694b0c3a14790104c57a022f33746e30fc2c2b90
keremcancetiz-git <keremcancetiz@gmail.com>
2026-04-29 12:39:04 +0200

    Delete ftir-controller-main directory

--- ftir-controller-main/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(['GET'])
-def home(request):
-    return Response({'function': 'health', 'message': 'Gooood morning vietnam!!'}, status=200)
