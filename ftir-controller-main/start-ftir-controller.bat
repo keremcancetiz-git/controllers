6f0e186cf3aca6e3ee94dd712ed81c0e4d6357ed
keremcancetiz-git <keremcancetiz@gmail.com>
2026-05-06 16:38:40 +0200

    Delete ftir-controller-main directory

--- ftir-controller-main/start-ftir-controller.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cd C:\Users\innoflex\Documents\ftir-controller
-py manage.py runserver 0.0.0.0:8080
-
-pause
--- ftir-controller-main/test.bat
+++
@@ -1,9 +0,0 @@
-curl.exe http://localhost:8080/health
-
-curl.exe http://localhost:8080/experiments/experiment-folders
-
-curl.exe -X POST http://localhost:8080/omnic/collect-bkg -H "Content-Type: application/json" -d "{\"experiment_name\":\"test\"}"
-
-curl.exe -X POST http://localhost:8080/omnic/collect-sample -H "Content-Type: application/json" -d "{\"experiment_name\":\"test\"}"
-
-pause
